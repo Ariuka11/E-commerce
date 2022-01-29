fc9deb29772bac73d32f486b4cbf53783117970c
Ariuka11 <ariun1118@gmail.com>
2022-01-29 17:21:49 -0500

    Finished navbar and slider

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
@@ -8,18 +10,69 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
+  align-items: center;
 `;
-const Left = styled.div``;
-const Center = styled.div``;
-const Right = styled.div``;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+const MenuItem = styled.div`
+  font-size: 14px;
+  margin-left: 25px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+`;
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
-        <Left>left</Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>ARIUKA.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
