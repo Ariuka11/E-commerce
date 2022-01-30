4d7ea0d68159bfdf31964628011876d2c70fdebd
Ariuka11 <ariun1118@gmail.com>
2022-01-29 22:04:22 -0500

    added newsletter

--- src/data.js
+++ src/data.js
@@ -67,7 +67,7 @@ export const popularProducts = [
   },
   {
     id: 7,
-    img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    img: "https://i.ibb.co/Y3RWtn9/shirt.png",
   },
   {
     id: 8,
